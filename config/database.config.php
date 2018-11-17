2b2ab2b401f7f6ef7d458941dd8bc374464a8af4
Owen Tsai <owentsai.v@gmail.com>
2018-11-17 20:15:55 +0800

    Init project structure

---
+++ config/database.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owen
+ * Date: 2018/11/17
+ * Time: 19:57
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "123456");
+define("DB_NAME", "db_envision");
---
+++ core/class.database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: Owen
+ * Date: 2018/11/17
+ * Time: 19:56
+ */
+
+class Database {
+
+}
